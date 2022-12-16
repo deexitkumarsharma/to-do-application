05be6946e50066decbadc8f598dae5a37ef2cb1c
Deexit Kumar Sharma <deexitkumarsharma@gmail.com>
2022-12-16 19:28:28 +0530

    fixed bugs

--- script.js
+++ script.js
@@ -1,52 +1,125 @@
-const itemsArray = localStorage.getItem("items")
-  ? JSON.parse(localStorage.getItem("items"))
-  : [];
+const taskInput = document.querySelector(".task-input input"),
+  filters = document.querySelectorAll(".filters span"),
+  clearAll = document.querySelector(".clear-btn"),
+  taskBox = document.querySelector(".task-box");
 
-document.querySelector("#enter").addEventListener("click", () => {
-  const item = document.querySelector("#item");
-  createItem(item);
+let editId,
+  isEditTask = false,
+  todos = JSON.parse(localStorage.getItem("todo-list"));
+
+filters.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector("span.active").classList.remove("active");
+    btn.classList.add("active");
+    showTodo(btn.id);
+  });
 });
 
-function displayItems() {
-  let items = "";
-  for (let index = 0; index < itemsArray.length; index++) {
-    items += `<div class="item">
-                <div class="input-controller">
-                <input type="checkbox" name="" id="task-1" />
-                    <label for="task-1" id="task-1">
-                        <span class="custom-checkbox"></span>
-                        ${itemsArray[index]}
-                        </label>
-                  <div class="fa-solid fa-delete-left deleteBtn">
-                  </div>
-                </div>
-              </div>`;
+showTodo("all");
+
+function showTodo(filter) {
+  let liTag = "";
+  if (todos) {
+    todos.forEach((todo, id) => {
+      console.log(id);
+      let completed = todo.status == "completed" ? "checked" : "";
+      if (filter == todo.status || filter == "all") {
+        liTag += `<li class="task">
+                            <label id="jalena" for=${id}>
+                                <input onclick="updateStatus(this)" type="checkbox" id=${id} id="jalena" ${completed}>
+                                <p class=${completed}>${todo.name}</p>
+                              </label>
+                            <button class="delete">delete </button>
+                        </li>`;
+      }
+    });
   }
 
-  document.querySelector(".to-do-list").innerHTML = items;
-  activateDeleteListeners();
+  taskBox.innerHTML = liTag || `<span>You don't have any task here</span>`;
+  let checkTask = taskBox.querySelectorAll(".task");
+  !checkTask.length
+    ? clearAll.classList.remove("active")
+    : clearAll.classList.add("active");
+  taskBox.offsetHeight >= 300
+    ? taskBox.classList.add("overflow")
+    : taskBox.classList.remove("overflow");
 }
 
-function activateDeleteListeners() {
-  let deleteBtn = document.querySelectorAll(".deleteBtn");
-  deleteBtn.forEach((dB, index) => {
-    dB.addEventListener("click", () => {
-      deleteItem(index);
-    });
+function showMenu(selectedTask) {
+  let menuDiv = selectedTask.parentElement.lastElementChild;
+  menuDiv.classList.add("show");
+  document.addEventListener("click", (e) => {
+    if (e.target.tagName != "I" || e.target != selectedTask) {
+      menuDiv.classList.remove("show");
+    }
   });
 }
 
-function createItem(item) {
-  itemsArray.push(item.value);
-  localStorage.setItem("items", JSON.stringify(itemsArray));
-  location.reload();
+function updateStatus(selectedTask) {
+  let taskName = selectedTask.parentElement.lastElementChild;
+  if (selectedTask.checked) {
+    taskName.classList.add("checked");
+    todos[selectedTask.id].status = "completed";
+  } else {
+    taskName.classList.remove("checked");
+    todos[selectedTask.id].status = "pending";
+  }
+  localStorage.setItem("todo-list", JSON.stringify(todos));
 }
 
-function deleteItem(index) {
-  itemsArray.splice(index, 1);
-  localStorage.setItem("items", JSON.stringify(itemsArray));
-  location.reload();
+// function editTask(taskId, textName) {
+//   editId = taskId;
+//   isEditTask = true;
+//   taskInput.value = textName;
+//   taskInput.focus();
+//   taskInput.classList.add("active");
+// }
+
+// delete events - used event bubbling in taskbox(parent)
+let taskbox = document.querySelector(".task-box");
+taskbox.addEventListener("click", deleteTask);
+function deleteTask(event) {
+  if (event.target.nodeName === "BUTTON") {
+    let taskli = document.querySelector(".task");
+    taskli.remove();
+  }
 }
-window.onload = function () {
-  displayItems();
-};
+
+taskbox.addEventListener("click", findCountOfUncheckedTasks);
+// count all pending task
+function findCountOfUncheckedTasks(e) {
+  let noOfUncheckedTasks = document.querySelector(".items-left");
+  let countOfUncheckedTasks = 0;
+  newInputs = document.querySelectorAll("input#jalena");
+
+  newInputs.forEach((input) => {
+    if (input.checked === false) {
+      countOfUncheckedTasks++;
+    }
+  });
+  noOfUncheckedTasks.innerText = countOfUncheckedTasks + " tasks left";
+}
+
+clearAll.addEventListener("click", () => {
+  isEditTask = false;
+  todos.splice(0, todos.length);
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+  showTodo();
+});
+
+taskInput.addEventListener("keyup", (e) => {
+  let userTask = taskInput.value.trim();
+  if (e.key == "Enter" && userTask) {
+    if (!isEditTask) {
+      todos = !todos ? [] : todos;
+      let taskInfo = { name: userTask, status: "pending" };
+      todos.push(taskInfo);
+    } else {
+      isEditTask = false;
+      todos[editId].name = userTask;
+    }
+    taskInput.value = "";
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo(document.querySelector("span.active").id);
+  }
+});
