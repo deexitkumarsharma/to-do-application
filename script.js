c7f925af01bfeaff975adb336ac554e7b96a3d3d
Deexit Kumar Sharma <deexitkumarsharma@gmail.com>
2022-12-17 18:20:08 +0530

    fixed bugs

--- script.js
+++ script.js
@@ -1,146 +1,392 @@
-const taskInput = document.querySelector(".task-input input"),
-  filters = document.querySelectorAll(".filters span"),
-  clearAll = document.querySelector(".clear-btn"),
-  taskBox = document.querySelector(".task-box");
+function Todo(id, content, isDone) {
+  this.id = id;
+  this.content = content;
+  this.isDone = isDone;
+}
 
-let editId,
-  isEditTask = false,
-  todos = JSON.parse(localStorage.getItem("todo-list"));
-// console.log(todos);
+/**
+ * Declare a controller
+ */
+function TodoController() {
+  this.todoList = [];
+  this.id = 1;
+  this.ENTER_KEY = 13;
+  this.todoInput = document.getElementById("newTodo");
+  this.todoListView = document.getElementById("todoListView");
+}
 
-filters.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    document.querySelector("span.active").classList.remove("active");
-    btn.classList.add("active");
-    showTodo(btn.id);
-  });
-});
+TodoController.prototype = {
+  getTodoFromLocalstorage: function (key) {
+    var todoList = JSON.parse(localStorage.getItem(key)) || [];
+    return todoList;
+  },
 
-showTodo("all");
+  setTodoLocalstorage: function (key) {
+    localStorage.setItem("todoList", JSON.stringify(key));
+  },
 
-function showTodo(filter) {
-  let liTag = "";
-  if (todos) {
-    todos.forEach((todo, id) => {
-      console.log(id);
-      let completed = todo.status == "completed" ? "checked" : "";
-      if (filter == todo.status || filter == "all") {
-        liTag += `<li class="task">
-                            <label for=${id}>
-                                <input onclick="updateStatus(this)" type="checkbox" id=${id} ${completed}>
-                                <p class=${completed}>${todo.name}</p>
-                              </label>
-                            <button class="delete">delete </button>
-                        </li>`;
+  handleTodoItem: function (value) {
+    this.isDone = false;
+    var mainArray = todoController.getTodoFromLocalstorage("todoList");
+    this.id = todoController.idLargestOfLocal(mainArray) + 1;
+    var todoItem = new Todo(this.id, value, this.isDone);
+    return todoItem;
+  },
+
+
+  idLargestOfLocal: function (mainArray) {
+    var lengthArr = mainArray.length;
+
+    if (lengthArr !== 0) {
+      return mainArray[lengthArr - 1].id;
+    } else {
+      return 0;
+    }
+    return lastId;
+  },
+
+  addNewTodo: function (todo, list) {
+    list.push(todo);
+    todoController.setTodoLocalstorage(list);
+    return todo;
+  },
+
+
+
+  setAttributes: function (element, attrs) {
+    for (var key in attrs) {
+      element.setAttribute(key, attrs[key]);
+    }
+  },
+
+
+ // Create new checkbox input element
+
+  checkboxView: function (todoId) {
+    var inpCheckbox = document.createElement("input");
+    this.setAttributes(inpCheckbox, {
+      type: "checkbox",
+      class: "itemList",
+      id: todoId,
+    });
+
+    //event check for input checkbox
+    inpCheckbox.addEventListener("click", function (e) {
+      //get list array from localStorage
+      var list = todoController.getTodoFromLocalstorage("todoList");
+      var id = e.target.getAttribute("id");
+      for (var i = 0; i < list.length; i++) {
+        if (list[i].id == id) {
+          list[i].isDone = e.target.checked;
+        }
+      }
+
+      //save list todo to localStorage
+      todoController.setTodoLocalstorage(list);
+      todoController.countItem();
+    });
+
+    return inpCheckbox;
+  },
+
+
+   // Create new lable element
+
+  createLableView: function (todo) {
+    var lbContent = document.createElement("label");
+    this.setAttributes(lbContent, {
+      value: todo.content,
+      class: "labelContent ",
+    });
+    lbContent.innerHTML = todo.content;
+
+    //return node lable
+    return lbContent;
+  },
+
+
+  initTodoITem: function (todo) {
+    var item = document.createElement("li");
+    item.setAttribute("class", "todoItem");
+    // this.setAttributes(item, { , class: 'todoItem ' });
+
+    //event event double click in node li
+    item.addEventListener("dblclick", function (e) {
+      item.classList.add("editing");
+    });
+
+    //return node li
+    return item;
+  },
+
+
+  editInputView: function (todo) {
+    //get array from localStorage
+    var list = todoController.getTodoFromLocalstorage("todoList");
+    var inputEdit = document.createElement("input");
+    this.setAttributes(inputEdit, {
+      id: todo.id,
+      class: "edit",
+      value: todo.content,
+      type: "text",
+    });
+    inputEdit.focus();
+
+    //event onblur get value edit and delete class editing when click outside this input
+    inputEdit.onblur = function (e) {
+      todoController.handleTodoUpdate(e);
+    };
+
+    //event onkeyup get value edit form inputEdit
+    inputEdit.onkeypress = function (e) {
+      if (
+        event.which == todoController.ENTER_KEY ||
+        event.keyCode == todoController.ENTER_KEY
+      ) {
+        todoController.handleTodoUpdate(e);
+      }
+    };
+
+    //return node input for edit todo
+    return inputEdit;
+  },
+
+  handleTodoUpdate: function (event) {
+    var list = todoController.getTodoFromLocalstorage("todoList");
+    var inputEdit = event.target;
+    var todoItem = new Todo(inputEdit.id, inputEdit.value, false);
+    todoController.updateTodoEdit(todoItem, list);
+    var editing = document.querySelector(".editing");
+    editing.classList.remove("editing");
+    todoController.renderTodo();
+  },
+
+
+  updateTodoEdit: function (todo, list) {
+    for (var i = 0; i < list.length; i++) {
+      if (list[i].id == todo.id) {
+        list[i].content = todo.content;
+        todoController.setTodoLocalstorage(list);
+        break;
+      }
+    }
+
+    //return new object have edit
+    return todo;
+  },
+
+
+  removeButtonView: function (todo) {
+    var btnRemove = document.createElement("button");
+    this.setAttributes(btnRemove, { class: "remove", id: todo.id });
+
+    //event click mouse into btnRemove a item todo
+    btnRemove.addEventListener("click", function (e) {
+      var id = e.target.getAttribute("id");
+      todoController.removeTodo(id);
+      todoController.renderTodo();
+      todoController.countItem();
+    });
+
+    //return node button
+    return btnRemove;
+  },
+
+
+  todoView: function (todo) {
+    var item = this.initTodoITem(todo); //create node li
+    var inpCheckbox = this.checkboxView(todo.id), //create node input checkbox
+      lbContent = this.createLableView(todo), //create node lable
+      inputEdit = this.editInputView(todo), //create node input edit
+      btnRemove = this.removeButtonView(todo); //create node button remove item todo
+
+    //item append each element
+    item.appendChild(inpCheckbox);
+    item.appendChild(lbContent);
+    item.appendChild(inputEdit);
+    item.appendChild(btnRemove);
+
+    //ul append each item
+    document.querySelector("#todoListView").appendChild(item);
+
+    //return node li contain inpCheckbox, lbContent, inputEdit, btnRemove
+    return item;
+  },
+
+
+  removeTodo: function (id, list) {
+    list = todoController.getTodoFromLocalstorage("todoList");
+    for (var i = 0; i < list.length; i++) {
+      if (list[i].id == id) {
+        list.splice(i, 1);
+        break;
+      }
+    }
+
+    //set value after remove item to localStorage
+    todoController.setTodoLocalstorage(list);
+  },
+
+
+  countItem: function (index, list) {
+    list = todoController.getTodoFromLocalstorage("todoList");
+    index = 0;
+    for (var i = 0; i < list.length; i++) {
+      if (!list[i].isDone) {
+        index++;
+      }
+    }
+
+    // return index display to UI;
+    document.getElementById("todoCount").innerHTML = index;
+  },
+
+  events: function () {
+    // Event add todo
+    todoController.todoInput.onkeyup = function (event) {
+      if (
+        event.which == todoController.ENTER_KEY ||
+        event.keyCode == todoController.ENTER_KEY
+      ) {
+        //get from localStorage
+        var todoList = todoController.getTodoFromLocalstorage("todoList");
+
+        //attach value for todo
+        var todoItem = todoController.handleTodoItem(
+          todoController.todoInput.value
+        );
+
+        //add new a Todo
+        var todo = todoController.addNewTodo(todoItem, todoList);
+
+        //Execute display to UI
+        todoController.todoView(todo);
+
+        //clear input
+        todoController.todoInput.value = "";
+        todoController.countItem();
+      }
+    };
+
+    //event check all checkbox in list item
+    var list = document.getElementsByClassName("itemList");
+    var checkAll = document.getElementById("toggleInputAll");
+    checkAll.addEventListener("change", function (e) {
+      var check;
+      for (var i = 0; i < list.length; i++) {
+        list[i].checked = this.checked;
+        check = e.target.checked;
+        todoController.checkAllTodo(check);
+      }
+
+      todoController.countItem();
+    });
+
+    //Show all items
+    var listWork = document.getElementsByClassName("todoItem");
+    var showAllItem = document.getElementById("allWorks");
+    showAllItem.addEventListener("click", function () {
+      for (var i = 0; i < listWork.length; i++) {
+        listWork[i].style.display = "block";
       }
     });
-    counter();
-  }
 
-  taskBox.innerHTML = liTag || `<span>You don't have any task here</span>`;
-  let checkTask = taskBox.querySelectorAll(".task");
-  !checkTask.length
-    ? clearAll.classList.remove("active")
-    : clearAll.classList.add("active");
-  taskBox.offsetHeight >= 300
-    ? taskBox.classList.add("overflow")
-    : taskBox.classList.remove("overflow");
-}
+    // Filter todo list with actived items
+    var activeItem = document.getElementsByClassName("todoItem");
+    var todoActive = document.getElementById("activedItems");
+    todoActive.addEventListener("click", function () {
+      for (var i = 0; i < list.length; i++) {
+        if (!list[i].checked) {
+          activeItem[i].style.display = "block";
+        } else {
+          activeItem[i].style.display = "none";
+        }
+      }
+    });
 
-function showMenu(selectedTask) {
-  let menuDiv = selectedTask.parentElement.lastElementChild;
-  menuDiv.classList.add("show");
-  document.addEventListener("click", (e) => {
-    if (e.target.tagName != "I" || e.target != selectedTask) {
-      menuDiv.classList.remove("show");
+    //Filter completed todo list
+    var completeItem = document.getElementsByClassName("todoItem");
+    var todoCompleted = document.getElementById("completedTodos");
+    todoCompleted.addEventListener("click", function () {
+      for (var i = 0; i < list.length; i++) {
+        if (list[i].checked) {
+          completeItem[i].style.display = "block";
+        } else {
+          completeItem[i].style.display = "none";
+        }
+      }
+    });
+
+    // Added event clear completed items for button
+    var clearButton = document.getElementById("btnClear");
+    clearButton.addEventListener("click", function () {
+      //get from localStorage
+      var list = todoController.getTodoFromLocalstorage("todoList");
+      todoController.clearCompleted(list);
+      todoController.setTodoLocalstorage(list);
+      todoController.renderTodo();
+    });
+  },
+
+
+  clearCompleted: function (list) {
+    while (list.find(({ isDone }) => isDone)) {
+      list.splice(list.indexOf(list.find(({ isDone }) => isDone)), 1);
     }
-  });
-  counter();
-}
+  },
 
-function updateStatus(selectedTask) {
-  let taskName = selectedTask.parentElement.lastElementChild;
-  if (selectedTask.checked) {
-    taskName.classList.add("checked");
-    todos[selectedTask.id].status = "completed";
-  } else {
-    taskName.classList.remove("checked");
-    todos[selectedTask.id].status = "pending";
-  }
-  counter();
-  localStorage.setItem("todo-list", JSON.stringify(todos));
-}
 
-// function editTask(taskId, textName) {
-//   editId = taskId;
-//   isEditTask = true;
-//   taskInput.value = textName;
-//   taskInput.focus();
-//   taskInput.classList.add("active");
-// }
-
-// delete events - used event ELEGATION in taskbox(parent)
-let taskbox = document.querySelector(".task-box");
-taskbox.addEventListener("click", deleteTask);
-
-function deleteTask(event) {
-  if (event.target.nodeName === "BUTTON") {
-    let taskli = document.querySelector(".task");
-    taskli.remove();
-  }
-  counter();
-}
-
-// count all pending task
-taskbox.addEventListener("click", counter());
-
-// function findCountOfUncheckedTasks(e) {
-//   let noOfUncheckedTasks = document.querySelector(".items-left");
-//   let countOfUncheckedTasks = 0;
-//   newInputs = document.querySelectorAll("#jalena");
-
-//   newInputs.forEach((input) => {
-//     if (input.checked === false) {
-//       countOfUncheckedTasks++;
-//     }
-//   });
-//   noOfUncheckedTasks.innerText = countOfUncheckedTasks + " tasks left";
-// }
-
-function counter() {
-  let res = document.querySelector(".items-left");
-  let c = 0;
-  todos.forEach((elem) => {
-    if (elem.status == "pending") {
-      c++;
+  checkAllTodo: function (check, todoList) {
+    todoList = todoController.getTodoFromLocalstorage("todoList");
+    for (var i = 0; i < todoList.length; i++) {
+      todoList[i].isDone = check;
+      todoController.setTodoLocalstorage(todoList);
     }
-  });
-  res.innerText = c + "items left";
+  },
+
+
+  renderTodo: function () {
+    //get from localStorage
+    var list = todoController.getTodoFromLocalstorage("todoList");
+    todoController.removeElement();
+    for (var i = 0; i < list.length; i++) {
+      var element = todoController.todoView(list[i]);
+      if (list[i].isDone) {
+        element.classList.add("checked");
+      }
+    }
+  },
+
+  removeElement: function () {
+    var todoListView = document.getElementById("todoListView");
+    while (todoListView.hasChildNodes()) {
+      todoListView.removeChild(todoListView.firstChild);
+    }
+  },
+};
+
+//change class selected
+function changeClass(elem) {
+  var a = document.getElementsByTagName("a");
+  for (var i = 0; i < a.length; i++) {
+    a[i].classList.remove("selected");
+  }
+
+  //add class selected for element user click
+  elem.classList.add("selected");
 }
 
-// clearAll
+//todoController handle all action add, delete, edit, events
+var todoController = new TodoController();
 
-clearAll.addEventListener("click", () => {
-  isEditTask = false;
-  todos.splice(0, todos.length);
-  localStorage.setItem("todo-list", JSON.stringify(todos));
-  showTodo();
-  counter();
-});
+//todo create new object todo
+var todo = new Todo();
 
-taskInput.addEventListener("keyup", (e) => {
-  let userTask = taskInput.value.trim();
-  if (e.key == "Enter" && userTask) {
-    if (!isEditTask) {
-      todos = !todos ? [] : todos;
-      let taskInfo = { name: userTask, status: "pending" };
-      todos.push(taskInfo);
-    } else {
-      isEditTask = false;
-      todos[editId].name = userTask;
-    }
-    taskInput.value = "";
-    localStorage.setItem("todo-list", JSON.stringify(todos));
-    showTodo(document.querySelector("span.active").id);
-  }
-});
+//performing the events
+todoController.events();
+
+//performing render todo display to UI
+todoController.renderTodo();
+
+//performing count all item active
+todoController.countItem();
